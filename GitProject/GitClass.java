54aeae8f6cae71ea92fd406e5b6861bf1244e30f
Kaushik030 <kaushik.143ap@gmail.com>
2015-04-13 11:54:15 +0530

    My 2nd Commit

---
+++ GitProject/GitClass.java
@@ -0,0 +1,14 @@
+package GitPackage;
+
+public class GitClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git I am BACK");
+		System.out.println("My second commit");
+
+	}
+
+}
